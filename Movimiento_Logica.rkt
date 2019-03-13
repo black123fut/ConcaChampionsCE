3bcbc9b5a3820d8f999983ccd180d010dc6b8887
gaburolo <38304056+gaburolo@users.noreply.github.com>
2019-03-12 22:18:00 -0600

    Movimiento/logica/angulo_final

--- Movimiento_Logica.rkt
+++ Movimiento_Logica.rkt
@@ -1,11 +1,18 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-advanced-reader.ss" "lang")((modname Movimiento_Logica) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname Movimiento_Logica) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
 ;;Agregar al codigo
 ;Argumentos: Listas Equipo1 y Equipo2
 ;Salida: Lista con ambos equipos y sus posiciones finales modificadas
 ;Funcion principal para mover los 2 equipos, llama a su debida funcion de movimiento
-(define (mover jugadores jugadores_S ball)
+(define (mover listaGrande)
+  (mover-aux (cadr (dividirEquipos (car listaGrande))) (car (dividirEquipos (car listaGrande))) (cadr listaGrande))
+  )
+
+;Argumentos: Listas Equipo1 y Equipo2
+;Salida: Lista con ambos equipos y sus posiciones finales modificadas
+;Funcion principal para mover los 2 equipos, llama a su debida funcion de movimiento
+(define (mover-aux jugadores jugadores_S ball)
   (cond((or (null? jugadores) (null? jugadores)) '())
        (else
         (list (mover_jugadores jugadores '() ball) (mover_jugadores_S jugadores_S '() ball)))))
@@ -66,20 +73,22 @@
 ;Salida: Tipo de movimiento a realizar o comprobar que tipo de jugador
 ;;Comprueba si un jugador del equipo 2 se puede mover hacia el balon
 (define (mover_a_bola_S  jugador listaF ball)
-
- (cond ((and (<= (abs (- (car(obtenerXY jugador)) (caar ball))) (* (obtenerAgilidad jugador) 15)) (<= (abs (- (cadr(obtenerXY jugador)) (cadar ball))) (* (obtenerAgilidad jugador) 15)) (comprobar_otro_jugador (car ball) listaF)) (comprobar_tipo_S jugador  (car ball)))
+ (cond ((and (<= (abs (- (car(obtenerXY jugador)) (caar ball))) (+ (* (obtenerAgilidad jugador) 15) 150))
+             (<= (abs (- (cadr(obtenerXY jugador)) (cadar ball))) (+ (* (obtenerAgilidad jugador) 15) 150))
+             (comprobar_otro_jugador (car ball) listaF))
+        (comprobar_tipo_S jugador  (car ball)))
         
-         ((equal? (obtenerNumTipo jugador) 1)
-          (moverPortero_S jugador (list 895 (+ (random 150) 180) )))
-         ((equal? (obtenerNumTipo jugador) 4)
-          (moverDelantero_S jugador (list  (+ (random 225) 5) (+ (random 505) 5) )))
+       ((equal? (obtenerNumTipo jugador) 1)
+        (moverPortero_S jugador (list 895 (+ (random 150) 180) )))
+       ((equal? (obtenerNumTipo jugador) 4)
+        (moverDelantero_S jugador (list  (+ (random 225) 5) (+ (random 505) 5) )))
+       
+       ((equal? (obtenerNumTipo jugador) 3)
+        (moverMedio_S jugador (list (+ (random 460) 230) (+ (random 505) 5) )))
+       ((equal? (obtenerNumTipo jugador) 2)
+        (moverDefensa_S jugador (list (+ (random 210) 690) (+ (random 505) 5) )))
          
-         ((equal? (obtenerNumTipo jugador) 3)
-          (moverMedio_S jugador (list (+ (random 460) 230) (+ (random 505) 5) )))
-         ((equal? (obtenerNumTipo jugador) 2)
-          (moverDefensa_S jugador (list (+ (random 210) 690) (+ (random 505) 5) )))
-         
-         ))
+       ))
 
 ;Argumentos: La lista de jugadores y una lista vacia
 ;Salida: Lista de jugadores con nueva posicion
@@ -139,37 +148,31 @@
 ;Funcion que verifica si un jugador puede moverse directamente a la bola 
 
 (define (mover_a_bola  jugador listaF ball)
-  
-
- (cond ((and (<= (abs (- (car(obtenerXY jugador)) (caar ball))) (* (obtenerAgilidad jugador) 15)) (<= (abs (- (cadr(obtenerXY jugador)) (cadar ball))) (* (obtenerAgilidad jugador) 15)) (comprobar_otro_jugador (car ball) listaF)) (comprobar_tipo jugador  (car ball)))
+ (cond ((and (<= (abs (- (car(obtenerXY jugador)) (caar ball))) (+ (* (obtenerAgilidad jugador) 15) 150))
+             (<= (abs (- (cadr(obtenerXY jugador)) (cadar ball))) (+ (* (obtenerAgilidad jugador) 15) 150))
+             (comprobar_otro_jugador (car ball) listaF))
+        (comprobar_tipo jugador  (car ball)))
         
-         ((equal? (obtenerNumTipo jugador) 1)
-          (moverPortero jugador (list 5 (+ (random 150) 180) )))
-         ((equal? (obtenerNumTipo jugador) 2)
-          (moverDefensa jugador (list  (+ (random 225) 5) (+ (random 505) 5) )))
+       ((equal? (obtenerNumTipo jugador) 1)
+        (moverPortero jugador (list 5 (+ (random 150) 180) )))
+       ((equal? (obtenerNumTipo jugador) 2)
+        (moverDefensa jugador (list  (+ (random 225) 5) (+ (random 505) 5) )))
          
-         ((equal? (obtenerNumTipo jugador) 3)
-          (moverMedio jugador (list (+ (random 460) 230) (+ (random 505) 5) )))
-         ((equal? (obtenerNumTipo jugador) 4)
-          (moverDelantero jugador (list (+ (random 210) 690) (+ (random 505) 5) )))
-         
-         ))
+       ((equal? (obtenerNumTipo jugador) 3)
+        (moverMedio jugador (list (+ (random 460) 230) (+ (random 505) 5) )))
+       ((equal? (obtenerNumTipo jugador) 4)
+        (moverDelantero jugador (list (+ (random 210) 690) (+ (random 505) 5) )))
+       ))
 
 ;Argumentos: Posicion deseada y lista nueva de jugadores
 ;Salida: #f si existe un jugador en esa posicion, #t si no hay ningun jugador en esa posicion
 ;Comprueba el si existe otro jugador en la posicion ya selecionada         
 (define (comprobar_otro_jugador posXYf listaF)
-
-  
   (cond((null? listaF) #t)
        ((equal? (obtenerXYf (car listaF)) posXYf) #f)
        (else
-        (begin
-          
-         (display posXYf)
-        (comprobar_otro_jugador posXYf (cdr listaF))))))
-
-
+       
+        (comprobar_otro_jugador posXYf (cdr listaF)))))
 
 ;Argumento: Jugador
 ;Salida:Caracteristica especifica del jugador
@@ -177,7 +180,7 @@
 ;;Funciones para obtener datos de un jugador en especifico
 ;;Obtiene las posiciones iniciales X y Y
 (define (obtenerXY jugador)
-  (cond((null? jugador) '())
+  (cond((or (null? jugador) (number? jugador)) '(50 50))
        (else
         (car jugador))))
 
@@ -269,61 +272,34 @@
   (cond((equal? num 4) (car jugador))
        (else
         (obtenerNum_aux (cdr jugador) (+ num 1)))))
+
+;;Divide la lista de jugadores en dos
+(define (dividirEquiposAux jugadores num nuevos)
+  (cond ((> num 10) (list nuevos jugadores))
+        (else (dividirEquiposAux (cdr jugadores) (+ num 1) (cons (car jugadores) nuevos)) ))
+  )
+
+(define (dividirEquipos jugadores)
+  (dividirEquiposAux jugadores 0 '() ))
+
 ;Obtiene el angulo para tirar segun su punteria
-(define (angulo jugador)
-  (cond ((null? jugador)
+(define (anguloTiro-aux jugador marco)
+  (begin
+    (display "Angulo: ")
+    (display (+ (* (/ (- (obtenerPunteria jugador) 10) -0.2222) (expt -1 (random 2))) marco)) (newline)
+    (display "Punteria: ")
+    (display (obtenerPunteria jugador)) (newline)
+    (cond ((null? jugador)
          '())
         (else
-         (* (* (obtenerPunteria jugador) (/ 9 10)) 10))))
+          (+ (* (/ (- (obtenerPunteria jugador) 10) -0.2222) (expt -1 (random 2))) marco)  )
+        )
+    ))
 
-;;creacion de los jugadores
-(define (jugador posXY posXYf estadisticas num-tipo numero)
-  (cond ((not(list? posXY)) #f)
-        
-        ((not (list? posXYf)) #f)
-        
-        ((not (list? estadisticas)) #f)
-        ((not (number? num-tipo)) #f)
-        ((not (number? numero)) #f)
+(define (anguloTiro jugador)
+  (cond ((equal? (obtenerEquipo jugador) "purple")
+         (anguloTiro-aux jugador (obtenerAngulo (list (obtenerXY jugador) '(0 250)))))
         (else
-         (list posXY posXYf estadisticas num-tipo numero))
-  ))
-
-
-
-;-------------------------------------------------------------FINALIZA EL CODIGO
-
-;;Pruebas de listas de jugadores
-
-
-(define (listajugadores  jugador1 jugador2 jugador3 jugador4 jugador5 jugador6 jugador7 jugador8 jugador9 jugador10 jugador11)
-  (list jugador1 jugador2 jugador3 jugador4 jugador5 jugador6 jugador7 jugador8 jugador9 jugador10 jugador11)
- )
-
-(define (jugadores)
-  (listajugadores (jugador '(100 30) '(200 6) '(8 5 10 4 1) 1 1)
-                  (jugador '(100 130) '(100 60) '(8 5 3 2 2) 2 2) (jugador '(150 60) '(100 60) '(8 5 3 2 5) 2 3) (jugador '(100 180) '(100 60) '(8 5 3 2 8) 2 4) (jugador '(100 60) '(100 60) '(8 5 3 2 10) 2 5)
-                  (jugador '(100 130) '(100 60) '(8 5 3 2 3) 3 6) (jugador '(100 60) '(199 11) '(8 5 3 2 6) 3 7) (jugador '(100 130) '(100 60) '(8 5 3 2 9) 3 8) (jugador '(100 60) '(100 60) '(8 5 3 2 11) 3 9)
-                  (jugador '(100 130) '(100 60) '(8 5 3 2 4) 4 10) (jugador '(100 180) '(100 60) '(8 5 3 2 7) 4 11))
- )
-
-(define (jugadores2)
-  (listajugadores (jugador '(100 30) '(200 6) '(8 5 3 4 1) 1 12)
-                  (jugador '(100 130) '(100 60) '(8 5 3 2 2) 2 13) (jugador '(150 60) '(100 60) '(8 5 3 2 5) 2 14) (jugador '(100 180) '(100 60) '(8 5 3 2 8) 2 15) (jugador '(100 60) '(100 60) '(8 5 3 2 10) 2 16)
-                  (jugador '(100 130) '(100 60) '(8 5 3 2 3) 2 17) (jugador '(100 60) '(199 11) '(8 5 3 2 6) 3 18) (jugador '(100 130) '(100 60) '(8 5 3 2 9) 3 19) (jugador '(100 60) '(100 60) '(8 5 3 2 11) 3 20)
-                  (jugador '(100 130) '(100 60) '(8 5 3 2 4) 3 21) (jugador '(100 180) '(100 60) '(8 5 3 2 7) 4 22))
- )
-
-
-
-
-
-(mover (jugadores) (jugadores2)  '((457 150) 1 1))
-
-
-
-
-
-;;(jugador 10 10 10 10 )
-;;(jugador 1 10 10 10 )
-;;(jugador '(10 60) 10 10 10 )
+         (anguloTiro-aux jugador (obtenerAngulo (list (obtenerXY jugador) '(920 250))))
+         ))
+  )
